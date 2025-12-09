ced8d856f82f122cb5b994704e85d92b6c6cb3d6
Angelo Galanti <angelo.galanti@gmail.com>
2025-12-09 16:05:56 +0100

    Remove obsolete scripts and correct API interaction errors in project management

--- exam/5m_v3/correzione/delbaldo.py
+++
@@ -1,7 +0,0 @@
-# Nessuna correzione sostanziale necessaria. Codice Eccellente.
-# Unico appunto:
-# Nella DELETE hai usato un query param:
-# requests.delete(f"{BASE_URL}/tasks?id={n_task}")
-#
-# Lo standard REST preferisce l'ID nel percorso:
-# requests.delete(f"{BASE_URL}/tasks/{n_task}")
